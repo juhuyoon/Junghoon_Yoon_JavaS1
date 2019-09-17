24e9ed7dac8a42be5479bd6f7a4ec15df7286072
Junghoon Yoon <juhuyoon@yahoo.com>
2019-09-17 13:10:39 -0400

    yaml and pivotal tracker finished, initiated spring directory

---
+++ JunghoonYoonU1Capstone/JunghoonYoonU1Capstone/src/main/java/com/company/JunghoonYoonU1Capstone/JunghoonYoonU1CapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.JunghoonYoonU1Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunghoonYoonU1CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunghoonYoonU1CapstoneApplication.class, args);
+	}
+
+}
